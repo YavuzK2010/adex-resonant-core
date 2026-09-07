84f0490605d59d2a4efc7efd298cd89488c14063
YavuzK2010 <yavuz2010kemal@gmail.com>
2026-09-07 10:33:46 +0300

    chore: clean up debug files, add .gitignore for generated symbols and exports

--- scripts/setup_project.py
+++
@@ -1,6 +0,0 @@
-import os
-
-ROOT = "/home/yavuzkemalinan/adex-resonant-brain"
-print("Setup starting...")
-# 1. Create custom symbol library
-sym_path = os.path.join(ROOT, "hardware", "symbols", "custom.kicad_sym")
